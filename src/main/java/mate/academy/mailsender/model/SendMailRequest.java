e100fe1978333950489c524fa44382a962cf7c05
Anatolii <dinexpod@gmail.com>
2019-04-06 12:23:37 +0300

    improve code readability

--- src/main/java/mate/academy/mailsender/model/SendMailRequest.java
+++ src/main/java/mate/academy/mailsender/model/SendMailRequest.java
@@ -1,15 +1,13 @@
 package mate.academy.mailsender.model;
 
-import lombok.Data;
-import org.thymeleaf.util.Validate;
-
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
 public class SendMailRequest {
 
-    @Valid @Size(min = 2, max = 120)
+    @Valid
+    @Size(min = 2, max = 120)
     private String title;
     @Email
     private String sendTo;
