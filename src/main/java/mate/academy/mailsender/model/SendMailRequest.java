5107f8a29c44f67922ebd6bb311b586345e76233
Anatolii <dinexpod@gmail.com>
2019-04-06 12:22:18 +0300

    added validator

--- src/main/java/mate/academy/mailsender/model/SendMailRequest.java
+++ src/main/java/mate/academy/mailsender/model/SendMailRequest.java
@@ -1,11 +1,17 @@
 package mate.academy.mailsender.model;
 
 import lombok.Data;
+import org.thymeleaf.util.Validate;
 
-@Data
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 public class SendMailRequest {
 
+    @Valid @Size(min = 2, max = 120)
     private String title;
+    @Email
     private String sendTo;
     private String message;
 }
