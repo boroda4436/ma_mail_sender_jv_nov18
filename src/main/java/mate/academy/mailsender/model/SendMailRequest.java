6eff61b02c7006f780d49a1630f678f79a67fdf1
Anatolii <dinexpod@gmail.com>
2019-04-06 12:34:44 +0300

    test

--- src/main/java/mate/academy/mailsender/model/SendMailRequest.java
+++ src/main/java/mate/academy/mailsender/model/SendMailRequest.java
@@ -1,9 +1,12 @@
 package mate.academy.mailsender.model;
 
+import lombok.Data;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
+@Data
 public class SendMailRequest {
 
     @Valid
